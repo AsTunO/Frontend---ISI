d727fb096af180df2c3c9f33d696676b8e5429b4
Júlio Cesar <juliocesar27072004@outlook.com>
2024-03-09 19:43:20 -0300

    feat: google charts

--- src/pages/dashboardPage/dashboardPage.jsx
+++ src/pages/dashboardPage/dashboardPage.jsx
@@ -1,11 +1,21 @@
 import "./dashboardPage.css"
 
+import LineChart from "../../components/Charts/lineChart.jsx"
+import SankeyChart from "../../components/Charts/sankeyChart.jsx"
+import BarChart from "../../components/Charts/barChart.jsx"
+
 function dashboardPage() {
 
-    return(
-        <div>
-            Dashboard
-        </div>
+    return (
+        <>
+            <div>
+                Dashboard
+            </div>
+
+            <LineChart />
+            <SankeyChart />
+            <BarChart />
+        </>
     )
 }
 
